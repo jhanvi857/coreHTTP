c566030a148f68771b64a606c4aeaf1259c398fb
jhanvi857 <jhanvip8507@gmail.com>
2026-02-18 20:18:34 +0530

    feat(routing): add router and route handler interface

--- src/main/java/com/jhanvi857/coreHTTP/routing/RouteHandler.java
+++ src/main/java/com/jhanvi857/coreHTTP/routing/RouteHandler.java
@@ -1,1 +1,9 @@
 package com.jhanvi857.coreHTTP.routing;
+
+import com.jhanvi857.coreHTTP.protocol.HttpRequest;
+import com.jhanvi857.coreHTTP.protocol.HttpResponse;
+import java.io.IOException;
+
+public interface RouteHandler {
+    HttpResponse handle(HttpRequest request) throws IOException;
+}
--- src/main/java/com/jhanvi857/coreHTTP/routing/Router.java
+++ src/main/java/com/jhanvi857/coreHTTP/routing/Router.java
@@ -1,1 +1,19 @@
 package com.jhanvi857.coreHTTP.routing;
+
+import com.jhanvi857.coreHTTP.protocol.HttpRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Router {
+    private final Map<String, RouteHandler> routes = new HashMap<>();
+
+    // registring route.
+    public void register(String path, RouteHandler handler) {
+        routes.put(path, handler);
+    }
+
+    // resolving route.
+    public RouteHandler resolve(HttpRequest request) {
+        return routes.get(request.getPath());
+    }
+}
